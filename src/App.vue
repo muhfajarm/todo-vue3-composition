695df61a860c4c9eb6fcd107370a448ee0b2eaed
MqD <muh.fajar12642@gmail.com>
2022-10-26 13:18:35 +0700

    Desain ToDo.

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,38 @@
 	<div class="container">
 		<div class="card">
 			<div class="card-body">
-				This is some text within a card body.
+				<h5 class="card-title text-center">Simple Todo</h5>
+
+				<div class="input-group mt-3">
+					<input type="text" class="form-control" placeholder="Type here...">
+					<button class="btn btn-primary" type="button" id="button-addon2">Add</button>
+				</div>
 			</div>
+		</div>
+
+		<div class="card my-3">
+			<ul class="list-group overflow-hidden">
+				<div class="d-flex justify-content-between align-items-center">
+					<li class="list-group-item w-100 border-0">An item</li>
+					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
+					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
+				</div>
+				<div class="d-flex justify-content-between align-items-center">
+					<li class="list-group-item w-100 border-0">An item</li>
+					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
+					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
+				</div>
+				<div class="d-flex justify-content-between align-items-center">
+					<li class="list-group-item w-100 border-0">An item</li>
+					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
+					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
+				</div>
+				<div class="d-flex justify-content-between align-items-center">
+					<li class="list-group-item w-100 border-0">An item</li>
+					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
+					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
+				</div>
+			</ul>
 		</div>
 	</div>
 </template>
