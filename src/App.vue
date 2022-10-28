0d3bfdd2c0e2bb5cc4c4f8e58a2fef2e893bc64c
MqD <muh.fajar12642@gmail.com>
2022-10-28 17:16:16 +0700

    learn3

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,79 @@
 				<h5 class="card-title text-center">Simple Todo</h5>
 
 				<div class="input-group mt-3">
-					<input type="text" class="form-control" placeholder="Type here...">
-					<button class="btn btn-primary" type="button" id="button-addon2">Add</button>
+					<input type="text" class="form-control" placeholder="Type here..." v-model="todo" @keyup.enter="addTodo()">
+					<button class="btn btn-primary" type="button" id="button-addon2" @click="addTodo()">Add</button>
 				</div>
 			</div>
 		</div>
 
-		<div class="card my-3">
-			<ul class="list-group overflow-hidden">
-				<div class="d-flex justify-content-between align-items-center">
-					<li class="list-group-item w-100 border-0">An item</li>
-					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
-					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
-				</div>
-				<div class="d-flex justify-content-between align-items-center">
-					<li class="list-group-item w-100 border-0">An item</li>
-					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
-					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
-				</div>
-				<div class="d-flex justify-content-between align-items-center">
-					<li class="list-group-item w-100 border-0">An item</li>
-					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
-					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
-				</div>
-				<div class="d-flex justify-content-between align-items-center">
-					<li class="list-group-item w-100 border-0">An item</li>
-					<button type="button" class="btn btn-sm btn-success mx-1 fw-bold">Done</button>
-					<button type="button" class="btn btn-sm btn-danger mx-1 fw-bold">X</button>
-				</div>
-			</ul>
+		<list :todos="todos" :total="totalTodo" @doneToDo="doneTodo" @deleteToDo="deleteTodo"></list>
+
+		<div
+			:class="totalTodo == 0 ? 'd-flex justify-content-between align-items-center my-3' : 'd-flex justify-content-between align-items-center'">
+			<small>Total ToDo : {{ totalTodo }}</small>
+			<button type="button" class="btn btn-sm btn-danger" v-if="totalTodo" @click="destroyToDo">
+				<i class="bi bi-trash"></i>
+			</button>
 		</div>
 	</div>
 </template>
+
+<script>
+import List from './components/List.vue';
+
+export default {
+	data() {
+		return {
+			todo: '',
+			todos: [],
+		}
+	},
+	mounted() {
+		this.todos = JSON.parse(localStorage.getItem('todos'))
+	},
+	components: { List },
+	methods: {
+		addTodo() {
+			this.todos.unshift({
+				activity: this.todo,
+				isDone: false,
+			})
+
+			this.todo = ''
+
+			this.saveLocalStorage()
+		},
+		doneTodo(indexTodo) {
+			this.todos[indexTodo].isDone = !this.todos[indexTodo].isDone
+
+			this.saveLocalStorage()
+		},
+		deleteTodo(indexTodo) {
+			this.todos = this.todos.filter((item, index) => {
+				if (index != indexTodo) {
+					return true
+				}
+			})
+
+			this.saveLocalStorage()
+		},
+		destroyToDo() {
+			this.todos = []
+
+			this.saveLocalStorage()
+		},
+		saveLocalStorage() {
+			localStorage.setItem('todos', JSON.stringify(this.todos))
+		}
+	},
+	computed: {
+		totalTodo() {
+			return this.todos.length
+		}
+	},
+}
+</script>
 
 <style>
 body {
---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="card my-3" v-if="total">
+		<ul class="list-group overflow-hidden">
+			<div class="d-flex justify-content-between align-items-center" v-for="(todo, index) in todos" :key="index">
+				<li class="list-group-item w-100 border-0">
+					<span v-if="todo.isDone">
+						<del>{{ todo.activity }}</del>
+					</span>
+					<span v-else>{{ todo.activity }}</span>
+				</li>
+
+				<button type="button" class="btn btn-sm btn-success mx-1" @click="doneToDo(index)">
+					<i class="bi bi-check"></i>
+				</button>
+				<button type="button" class="btn btn-sm btn-danger mx-1" @click="deleteToDo(index)">
+					<i class="bi bi-x"></i>
+				</button>
+			</div>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		todos: {
+			type: Array,
+			default: [],
+		},
+		total: Number,
+	},
+	methods: {
+		doneToDo(index) {
+			this.$emit('doneToDo', index)
+		},
+		deleteToDo(index) {
+			this.$emit('deleteToDo', index)
+		}
+	},
+}
+</script>
